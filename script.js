89dfc02f04a50301fff60553a6a7b392a1465ef1
joona-3 <52347755+joona-3@users.noreply.github.com>
2023-10-13 09:05:51 +0200

    add suntime logic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const containerMain = document.getElementById("containerMain");
 const currentTemperature = document.getElementById("currentTemperature");
 const currentCity = document.getElementById("currentCity");
 const currentWeatherType = document.getElementById("currentWeatherType");
+const sunTime = document.getElementById("sunTime");
 
 // API URL
 const url = "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=903ddcd89f258c2d7a183180e626b811";
@@ -14,18 +15,20 @@ const fetchWeatherData = () => {
             return response.json()
         })
         .then((weatherData) => {
-            console.log("console", weatherData)
-
             // Save data in variables
             const cityTemp = Math.round(weatherData.main.temp * 10) / 10;
             const cityName = weatherData.name;
             const weatherType = weatherData.weather.map((weatherType) => weatherType.description);
+            const sunRise = new Date(weatherData.sys.sunrise * 1000);
+            const sunSet = new Date(weatherData.sys.sunset * 1000);
 
             // Display values in HTML
             currentTemperature.textContent = cityTemp;
             currentCity.textContent = cityName;
             currentWeatherType.textContent = weatherType;
-
+            sunTime.innerHTML = `<p>sunrise</p>
+            <p>${sunRise.getHours()}:${sunRise.getMinutes()}</p>
+            <p>sunset</p><p>${sunSet.getHours()}:${sunSet.getMinutes()}</p>`;
 
 
         })
